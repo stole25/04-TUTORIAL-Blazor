e49ae018f132688f2d25e86ecc9ea23f2d66ad7b
stole25 <119949884+stole25@users.noreply.github.com>
2024-12-12 18:21:31 +0100

    songs dio

--- SongListApp/Data/AppDbContext.cs
+++ SongListApp/Data/AppDbContext.cs
@@ -2,12 +2,9 @@ using Microsoft.EntityFrameworkCore;
 
 namespace SongListApp.Data;
 
-public class AppDbContext
-{
     public class AppDbContext : DbContext
     {
         public AppDbContext(DbContextOptions<AppDbContext> options) :
             base(options) { }
         public DbSet<Song> Songs { get; set; }
     }
-}
